cb74a4232589050b1a2743b304b06def526a3065
shantanaBernados <shantana.bernados@gmail.com>
2015-03-31 11:19:59 +0800

    for buildout

--- django-facebook/setup.py
+++ django-facebook/setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "django-facebook",
-    version = "1.0",
-    url = 'http://github.com/shantanaBernados/django-facebook',
-    license = 'BSD',
-    description = "A short URL handler for Django apps.",
-    author = 'Shantana Bernados',
-    packages = find_packages('src'),
-    package_dir = {'': 'src'},
-    install_requires = ['setuptools'],
+    name="django-facebook",
+    version="1.0",
+    url='https://github.com/shantanaBernados/facebook_wall',
+    license='BSD',
+    description="A mini project for training.",
+    author='Shantana Bernados',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=['setuptools'],
 )
